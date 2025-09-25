23409952225069a15017dccd7fa6c7fa0d99fd36
Trung Nguyen <nttrungmt@gmail.com>
2025-09-25 11:55:44 -0500

    Create Employee.java
    Add Employee.java for lab05

---
+++ Employee.java
@@ -0,0 +1,24 @@
+package lab05;
+
+public class Employee {
+    private int ssn;            //the SSN of this employee
+    private String name;        //name of the employee
+    private double salary;      //salary of the employee
+
+    //Constructor method
+    public Employee(int ssn, String name, double salary) {
+        this.ssn = ssn;
+        this.name = name;
+        this.salary = salary;
+    }
+    
+    //get methods
+    public int getSsn()       { return ssn;     }
+    public String getName()   { return name;    }
+    public double getSalary() { return salary;  }
+
+    //Overrided toString() method
+    public String toString() {
+        return "ssn: " + ssn + ", name: " + name + ", salary: " + salary;
+    }
+}
